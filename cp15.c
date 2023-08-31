32c59e02d233e656d057b974ad44462d9b147785
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-31 22:03:24 +0530

    Add files via upload

---
+++ cp15.c
@@ -0,0 +1,11 @@
+// Write program to calculate the area and perimeter of a circle by reading the radius
+#include<stdio.h>
+int main()
+{
+	float r,a,p,pi=3.142;
+	printf("Enter the radius of the circle: ");
+	scanf("%f",&r);
+	a=pi*r*r;
+	p=2*pi*r;
+	printf("Area = %.2f square units\nPerimeter = %.2f units",a,p);
+}
