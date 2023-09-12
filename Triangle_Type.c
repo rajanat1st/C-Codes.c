c37f19f3a4462182ca7405b076d52cd08245b840
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-12 23:12:43 +0530

    Add files via upload

---
+++ Triangle_Type.c
@@ -0,0 +1,19 @@
+// Write a program to check whether a triangle is Equilateral, Isosceles or Scalene.
+
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the three sides of a triangle: ");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a+b>c && a+c>b && b+c>a)
+    {
+	if(a==b && b==c)
+	printf("Equilateral Triangle");
+	else if(a==b || a==c || b==c)
+	printf("Isosceles Triangle");
+	else
+	printf("Scalene Triangle");}
+	else
+	printf("Triangle does not exist!");
+}
