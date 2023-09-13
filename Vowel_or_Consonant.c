6fa28c73f0e54543d7284be588b8982a70e59902
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-13 21:31:39 +0530

    Add files via upload

---
+++ Vowel_or_Consonant.c
@@ -0,0 +1,13 @@
+// Find out whether the given character is Vowel or Consonant.
+
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter a character: ");
+	scanf("%c",&ch);
+	if(ch=='a'|| ch=='A'|| ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o'|| ch=='O' || ch=='u' || ch=='U')
+	printf("'%c' is a Vowel",ch);
+	else
+    printf("'%c' is a Consonant",ch);
+}
