99b676c5b410b15a86f239918acdb59fda794464
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-26 11:47:14 +0530

    Add files via upload

---
+++ All_factors_of_a_number.c
@@ -0,0 +1,14 @@
+// Write a program to print all factors of a number.
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	printf("Factors of %d: ",n);
+	for(int i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		printf("%d ",i);
+	}
+}
