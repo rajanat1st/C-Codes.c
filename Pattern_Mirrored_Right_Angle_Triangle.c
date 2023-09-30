6ae8eac8535554b366ec641d765fc49ff415d645
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-30 13:05:45 +0530

    Pattern_Mirrored_Right_Angle_Triangle.c

---
+++ Pattern_Mirrored_Right_Angle_Triangle.c
@@ -0,0 +1,21 @@
+// Print Mirrored Right angle Triangle pattern with stars.
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number of Rows: ");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{   // i^th row has (n-i) leading spaces
+		for(int j=1;j<=n-i;j++)
+		{
+			printf("  ");
+		}
+		// i^th row has i elements
+		for(int j=1;j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
