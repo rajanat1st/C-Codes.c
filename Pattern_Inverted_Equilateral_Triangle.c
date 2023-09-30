e2a5eefe04b4479b79f97b9fc365c3b1b776f7c4
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-30 12:59:16 +0530

    Pattern_Inverted_Equilateral_Triangle.c

--- Pattern_Inverted_Equilateral_Triangle.c
+++ Pattern_Inverted_Equilateral_Triangle.c
@@ -1,16 +1,21 @@
 // Print Inverted Equilateral Triangle pattern with stars.
-#include<stdio.h>
 int main()
 {
 	int n;
 	printf("Enter the number of Rows: ");
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++)
-	{
+	{    
+		// i^th row has (i-1) leading spaces
 		for(int j=1;j<=i-1;j++)
-		{ printf(" "); }
+		{
+			printf(" ");
+		}
+		// i^th row has (n-i+1) elements
 		for(int j=1;j<=n-i+1;j++)
-		{ printf("* ");}
-		printf("\n"); 
+		{
+			printf("* ");
+		} 
+		printf("\n");
 	}
 }
