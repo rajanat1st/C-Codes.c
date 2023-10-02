10f84c169f69da230860019e23fd63f26ce393d4
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-02 23:35:45 +0530

    Pattern_Right_Bent_Equilateral_Triangle.c

--- Pattern_Right_Bent_Equilateral_Triangle.c
+++ Pattern_Right_Bent_Equilateral_Triangle.c
@@ -1,17 +1,29 @@
-// Print Right bent Equilateral Triangle with stars.
+// Print Right bent Equilateral Triangle pattern with stars.
 #include<stdio.h>
-//#include<stdlib.h>
 int main()
 {
 	int n;
 	printf("Enter the number of Rows: ");
 	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
+	// Right bent Equilateral Triangle has n+(n-1) rows
+	for(int i=1;i<=n+(n-1);i++)
 	{    
-	    // i^th row has n-|n-i| elements
+        // i^th row has n-|n-i| elements and spaces together
 		for(int j=1;j<=n-abs(n-i);j++)
 		{
-			printf("* ");
+		   // For i, Odd row starts with element and Even row starts with space
+		   if(i%2!=0)
+		   {
+		      if(j%2!=0)
+			printf("*  ");
+		      else
+			printf("   ");
+		    }
+		    else
+		      if(j%2!=0)
+		       printf("   ");
+		      else
+		       printf("*  ");
 		}
 		printf("\n");
 	}
