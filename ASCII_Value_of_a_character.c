110a951919dd50e72c3422297d5864e11e182c47
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-03 23:57:58 +0530

    ASCII_Value_of_a_character.c

---
+++ ASCII_Value_of_a_character.c
@@ -0,0 +1,9 @@
+// Program to find ASCII Value of a character.
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter a character: ");
+	scanf("%c",&ch);
+	printf("The ASCII value of %c is %d",ch,ch);
+}
