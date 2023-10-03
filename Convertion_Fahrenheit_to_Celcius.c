fe01a98bab3217b347d28df4f567af74d2507800
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-03 23:56:12 +0530

    Convertion_Fahrenheit_to_Celcius.c

---
+++ Convertion_Fahrenheit_to_Celcius.c
@@ -0,0 +1,10 @@
+// Program to convert Fahrenheit to Celcius.
+#include<stdio.h>
+int main()
+{
+	float f,c;
+	printf("Enter Temperature in Fahrenheit: ");
+	scanf("%f",&f);
+	c=(f-32)*5/9;
+	printf("Temperature in Degree Celcius = %.2f",c);
+}
