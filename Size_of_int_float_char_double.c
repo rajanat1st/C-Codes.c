59911e04c569cc4bbbafefc9f75be49fd480e35c
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-06 23:26:03 +0530

    Add files via upload

---
+++ Size_of_int_float_char_double.c
@@ -0,0 +1,13 @@
+// Write C progrm to find the Size of int, float, char, double.
+#include<stdio.h>
+int main()
+{
+	int n;
+	float f;
+	char ch;
+	double d;
+	printf("The Size of int : %d",sizeof(n));
+    printf("\nThe Size of float : %d",sizeof(f));
+    printf("\nThe Size of char : %d",sizeof(ch));
+    printf("\nThe Size of int : %d",sizeof(d));
+}
